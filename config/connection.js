49c8580e8e60b5b232796ce9b7a5e3a5d5485e0c
George Greene <georgegreene@outlook.com>
2018-03-31 16:28:57 -0400

    jawsDB

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,19 @@
 var mysql = require("mysql");
 
 // Here, we define the parameters for our connection to the database.
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
   password: "root",
   database: "burgers_db"
 });
+}
 
 // Here, we initialize our connection.
 connection.connect(function(err) {
